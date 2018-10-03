9ea77680022fdb902179df826d99b428e68d81d0
Keren-Happuc Fleurimond <keren@noukod.com>
2018-10-03 18:16:44 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,22 @@
 # Your code goes here!
 class Anagram
 
-  attr_accessor :word
-  def initialize(word)
-    @word = word
+  # attr_accessor :word
+  # def initialize(word)
+  #   @word = word
+  # end
+  # 
+  # def match(anaword)
+  #   anaword.select do |word|
+  #   word.split("").sort == @word.split("").sort
+  #   end
+  # end
+  def combine_anagrams(words)
+    anagrams={}
+        words.each do |word|
+          anagrams[word.downcase.split('').sort.join] ||=[]
+          anagrams[word.downcase.split('').sort.join] << word 
+        end
+        anagrams.values
   end
-
-  def match(anaword)
-    anaword.select do |word|
-    word.split("").sort == @word.split("").sort
-    end
-  end
-
 end
