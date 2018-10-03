024066b93fd8966a76ef1470c2396cb8d500910f
Keren-Happuc Fleurimond <keren@noukod.com>
2018-10-03 18:24:07 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,9 +1,9 @@
 # Your code goes here!
 class Anagram
 
-  attr_accessor :word
-  def initialize(word)
-    @word = word
+  attr_accessor :anagram
+  def initialize(anagram)
+    @anagram = anagram
   end
 
   def match(anaword)
