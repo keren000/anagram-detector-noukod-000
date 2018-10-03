b494868cda611bd9b1d7c235c473c95a6f955f60
Keren-Happuc Fleurimond <keren@noukod.com>
2018-10-03 18:05:57 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(words)
+    word.select do |word|
+    word.split("").sort == @word.split("").sort
+    word.chars.sort == @anagram.chars.sort
+  end
+
+end
